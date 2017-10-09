d8145fca60ff35e2cdc3baf41a653fdff40e1f47
Larry Miles <lmiles@lundinoir.com>
2017-10-09 13:48:49 +0100

    your details validation commit

--- app/routes.js
+++ app/routes.js
@@ -23,17 +23,6 @@ router.post('/start-page', function (req, res) {
 
 })
 
-// Your details
-router.post('/your-details', function (req, res) {
-
-  var youDetails = "1"
-
-  if (youDetails == "1"){
-    res.redirect('/your-plea')
-  }
-
-})
-
 // Find your case
 router.post('/find-your-case', function (req, res) {
 
@@ -66,7 +55,7 @@ router.post('/find-your-case-error-1', function (req, res) {
 router.post('/find-your-case-error-2', function (req, res) {
 
   var caseFound = "1"
-  var caseURNValidation  = req.session.data['case-urn-number']
+  var caseURNValidation = req.session.data['case-urn-number']
   
   if (caseFound == "1" && caseURNValidation == "123"){
       res.redirect('/find-your-case-error-1')
@@ -76,6 +65,326 @@ router.post('/find-your-case-error-2', function (req, res) {
       res.redirect('/your-details')
   }
 
+})
+
+// Your details
+router.post('/your-details', function (req, res) {
+
+  var youDetails = "1"
+  var nameAddressGroup = req.session.data['name-address-group']
+
+  if (nameAddressGroup !== "1") {
+    if (nameAddressGroup !== "2") {
+        res.redirect('/your-details-error-0')
+    }
+  }
+    
+  if (req.session.data['home-telephone'] == "") {
+      req.session.data['home-telephone'] = "–"
+  }
+  if (req.session.data['mobile'] == "") {
+      req.session.data['mobile'] = "–"
+  }
+  if (req.session.data['email'] == "") {
+      res.redirect('/your-details-error-1')
+  }
+  if (req.session.data['dob-day'] == "") {
+      res.redirect('/your-details-error-2')
+  }
+    
+  if (req.session.data['dob-month'] == "") {
+      res.redirect('/your-details-error-2')
+  } else if ((req.session.data['dob-month'] == "1") || (req.session.data['dob-month'] == "01")) {
+      req.session.data['dob-month'] = "January"
+  } else if ((req.session.data['dob-month'] == "2") || (req.session.data['dob-month'] == "02")) {
+      req.session.data['dob-month'] = "Februrary"
+  } else if ((req.session.data['dob-month'] == "3") || (req.session.data['dob-month'] == "03")) {
+      req.session.data['dob-month'] = "March"
+  } else if ((req.session.data['dob-month'] == "4") || (req.session.data['dob-month'] == "04")) {
+      req.session.data['dob-month'] = "April"
+  } else if ((req.session.data['dob-month'] == "5") || (req.session.data['dob-month'] == "05")) {
+      req.session.data['dob-month'] = "May"
+  } else if ((req.session.data['dob-month'] == "6") || (req.session.data['dob-month'] == "06")) {
+      req.session.data['dob-month'] = "June"
+  } else if ((req.session.data['dob-month'] == "7") || (req.session.data['dob-month'] == "07")) {
+      req.session.data['dob-month'] = "July"
+  } else if ((req.session.data['dob-month'] == "8") || (req.session.data['dob-month'] == "08")) {
+      req.session.data['dob-month'] = "August"
+  } else if ((req.session.data['dob-month'] == "9") || (req.session.data['dob-month'] == "09")) {
+      req.session.data['dob-month'] = "September"
+  } else if (req.session.data['dob-month'] == "10") {
+      req.session.data['dob-month'] = "October"
+  } else if (req.session.data['dob-month'] == "11") {
+      req.session.data['dob-month'] = "November"
+  } else if (req.session.data['dob-month'] == "12") {
+      req.session.data['dob-month'] = "December"
+ }
+    
+  if (req.session.data['dob-year'] == "") {
+      res.redirect('/your-details-error-2')
+  }
+  if (req.session.data['NIN'] == "") {
+      res.redirect('/your-details-error-3')
+  }
+
+  if (youDetails == "1"){
+    res.redirect('/your-plea')
+  }
+})
+router.post('/your-details-error-0', function (req, res) {
+
+  var youDetails = "1"
+  var nameAddressGroup = req.session.data['name-address-group']
+
+  if (nameAddressGroup !== "1") {
+    if (nameAddressGroup !== "2") {
+        res.redirect('/your-details-error-0')
+    }
+  }
+    
+  if (req.session.data['home-telephone'] == "") {
+      req.session.data['home-telephone'] = "–"
+  }
+  if (req.session.data['mobile'] == "") {
+      req.session.data['mobile'] = "–"
+  }
+  if (req.session.data['email'] == "") {
+      res.redirect('/your-details-error-1')
+  }
+  if (req.session.data['dob-day'] == "") {
+      res.redirect('/your-details-error-2')
+  }
+    
+  if (req.session.data['dob-month'] == "") {
+      res.redirect('/your-details-error-2')
+  } else if ((req.session.data['dob-month'] == "1") || (req.session.data['dob-month'] == "01")) {
+      req.session.data['dob-month'] = "January"
+  } else if ((req.session.data['dob-month'] == "2") || (req.session.data['dob-month'] == "02")) {
+      req.session.data['dob-month'] = "Februrary"
+  } else if ((req.session.data['dob-month'] == "3") || (req.session.data['dob-month'] == "03")) {
+      req.session.data['dob-month'] = "March"
+  } else if ((req.session.data['dob-month'] == "4") || (req.session.data['dob-month'] == "04")) {
+      req.session.data['dob-month'] = "April"
+  } else if ((req.session.data['dob-month'] == "5") || (req.session.data['dob-month'] == "05")) {
+      req.session.data['dob-month'] = "May"
+  } else if ((req.session.data['dob-month'] == "6") || (req.session.data['dob-month'] == "06")) {
+      req.session.data['dob-month'] = "June"
+  } else if ((req.session.data['dob-month'] == "7") || (req.session.data['dob-month'] == "07")) {
+      req.session.data['dob-month'] = "July"
+  } else if ((req.session.data['dob-month'] == "8") || (req.session.data['dob-month'] == "08")) {
+      req.session.data['dob-month'] = "August"
+  } else if ((req.session.data['dob-month'] == "9") || (req.session.data['dob-month'] == "09")) {
+      req.session.data['dob-month'] = "September"
+  } else if (req.session.data['dob-month'] == "10") {
+      req.session.data['dob-month'] = "October"
+  } else if (req.session.data['dob-month'] == "11") {
+      req.session.data['dob-month'] = "November"
+  } else if (req.session.data['dob-month'] == "12") {
+      req.session.data['dob-month'] = "December"
+ }
+    
+  if (req.session.data['dob-year'] == "") {
+      res.redirect('/your-details-error-2')
+  }
+  if (req.session.data['NIN'] == "") {
+      res.redirect('/your-details-error-3')
+  }
+
+  if (youDetails == "1"){
+    res.redirect('/your-plea')
+  }
+})
+router.post('/your-details-error-1', function (req, res) {
+
+  var youDetails = "1"
+  req.session.data['name-address-group'] = ""
+  var nameAddressGroup = req.session.data['name-address-group']
+
+  if (nameAddressGroup !== "1") {
+    if (nameAddressGroup !== "2") {
+        res.redirect('/your-details-error-0')
+    }
+  }
+    
+  if (req.session.data['home-telephone'] == "") {
+      req.session.data['home-telephone'] = "–"
+  }
+  if (req.session.data['mobile'] == "") {
+      req.session.data['mobile'] = "–"
+  }
+  if (req.session.data['email'] == "") {
+      res.redirect('/your-details-error-1')
+  }
+  if (req.session.data['dob-day'] == "") {
+      res.redirect('/your-details-error-2')
+  }
+    
+  if (req.session.data['dob-month'] == "") {
+      res.redirect('/your-details-error-2')
+  } else if ((req.session.data['dob-month'] == "1") || (req.session.data['dob-month'] == "01")) {
+      req.session.data['dob-month'] = "January"
+  } else if ((req.session.data['dob-month'] == "2") || (req.session.data['dob-month'] == "02")) {
+      req.session.data['dob-month'] = "Februrary"
+  } else if ((req.session.data['dob-month'] == "3") || (req.session.data['dob-month'] == "03")) {
+      req.session.data['dob-month'] = "March"
+  } else if ((req.session.data['dob-month'] == "4") || (req.session.data['dob-month'] == "04")) {
+      req.session.data['dob-month'] = "April"
+  } else if ((req.session.data['dob-month'] == "5") || (req.session.data['dob-month'] == "05")) {
+      req.session.data['dob-month'] = "May"
+  } else if ((req.session.data['dob-month'] == "6") || (req.session.data['dob-month'] == "06")) {
+      req.session.data['dob-month'] = "June"
+  } else if ((req.session.data['dob-month'] == "7") || (req.session.data['dob-month'] == "07")) {
+      req.session.data['dob-month'] = "July"
+  } else if ((req.session.data['dob-month'] == "8") || (req.session.data['dob-month'] == "08")) {
+      req.session.data['dob-month'] = "August"
+  } else if ((req.session.data['dob-month'] == "9") || (req.session.data['dob-month'] == "09")) {
+      req.session.data['dob-month'] = "September"
+  } else if (req.session.data['dob-month'] == "10") {
+      req.session.data['dob-month'] = "October"
+  } else if (req.session.data['dob-month'] == "11") {
+      req.session.data['dob-month'] = "November"
+  } else if (req.session.data['dob-month'] == "12") {
+      req.session.data['dob-month'] = "December"
+ }
+    
+  if (req.session.data['dob-year'] == "") {
+      res.redirect('/your-details-error-2')
+  }
+  if (req.session.data['NIN'] == "") {
+      res.redirect('/your-details-error-3')
+  }
+
+  if (youDetails == "1"){
+    res.redirect('/your-plea')
+  }
+})
+router.post('/your-details-error-2', function (req, res) {
+
+  var youDetails = "1"
+  req.session.data['name-address-group'] = ""
+  var nameAddressGroup = req.session.data['name-address-group']
+
+  if (nameAddressGroup !== "1") {
+    if (nameAddressGroup !== "2") {
+        res.redirect('/your-details-error-0')
+    }
+  }
+    
+  if (req.session.data['home-telephone'] == "") {
+      req.session.data['home-telephone'] = "–"
+  }
+  if (req.session.data['mobile'] == "") {
+      req.session.data['mobile'] = "–"
+  }
+  if (req.session.data['email'] == "") {
+      res.redirect('/your-details-error-1')
+  }
+  if (req.session.data['dob-day'] == "") {
+      res.redirect('/your-details-error-2')
+  }
+    
+  if (req.session.data['dob-month'] == "") {
+      res.redirect('/your-details-error-2')
+  } else if ((req.session.data['dob-month'] == "1") || (req.session.data['dob-month'] == "01")) {
+      req.session.data['dob-month'] = "January"
+  } else if ((req.session.data['dob-month'] == "2") || (req.session.data['dob-month'] == "02")) {
+      req.session.data['dob-month'] = "Februrary"
+  } else if ((req.session.data['dob-month'] == "3") || (req.session.data['dob-month'] == "03")) {
+      req.session.data['dob-month'] = "March"
+  } else if ((req.session.data['dob-month'] == "4") || (req.session.data['dob-month'] == "04")) {
+      req.session.data['dob-month'] = "April"
+  } else if ((req.session.data['dob-month'] == "5") || (req.session.data['dob-month'] == "05")) {
+      req.session.data['dob-month'] = "May"
+  } else if ((req.session.data['dob-month'] == "6") || (req.session.data['dob-month'] == "06")) {
+      req.session.data['dob-month'] = "June"
+  } else if ((req.session.data['dob-month'] == "7") || (req.session.data['dob-month'] == "07")) {
+      req.session.data['dob-month'] = "July"
+  } else if ((req.session.data['dob-month'] == "8") || (req.session.data['dob-month'] == "08")) {
+      req.session.data['dob-month'] = "August"
+  } else if ((req.session.data['dob-month'] == "9") || (req.session.data['dob-month'] == "09")) {
+      req.session.data['dob-month'] = "September"
+  } else if (req.session.data['dob-month'] == "10") {
+      req.session.data['dob-month'] = "October"
+  } else if (req.session.data['dob-month'] == "11") {
+      req.session.data['dob-month'] = "November"
+  } else if (req.session.data['dob-month'] == "12") {
+      req.session.data['dob-month'] = "December"
+ }
+    
+  if (req.session.data['dob-year'] == "") {
+      res.redirect('/your-details-error-2')
+  }
+  if (req.session.data['NIN'] == "") {
+      res.redirect('/your-details-error-3')
+  }
+
+  if (youDetails == "1"){
+    res.redirect('/your-plea')
+  }
+})
+router.post('/your-details-error-3', function (req, res) {
+
+  var youDetails = "1"
+  req.session.data['name-address-group'] = ""
+  var nameAddressGroup = req.session.data['name-address-group']
+
+  if (nameAddressGroup !== "1") {
+    if (nameAddressGroup !== "2") {
+        res.redirect('/your-details-error-0')
+    }
+  }
+    
+  if (req.session.data['home-telephone'] == "") {
+      req.session.data['home-telephone'] = "–"
+  }
+  if (req.session.data['mobile'] == "") {
+      req.session.data['mobile'] = "–"
+  }
+  if (req.session.data['email'] == "") {
+      res.redirect('/your-details-error-1')
+  }
+  if (req.session.data['dob-day'] == "") {
+      res.redirect('/your-details-error-2')
+  }
+    
+  if (req.session.data['dob-month'] == "") {
+      res.redirect('/your-details-error-2')
+  } else if ((req.session.data['dob-month'] == "1") || (req.session.data['dob-month'] == "01")) {
+      req.session.data['dob-month'] = "January"
+  } else if ((req.session.data['dob-month'] == "2") || (req.session.data['dob-month'] == "02")) {
+      req.session.data['dob-month'] = "Februrary"
+  } else if ((req.session.data['dob-month'] == "3") || (req.session.data['dob-month'] == "03")) {
+      req.session.data['dob-month'] = "March"
+  } else if ((req.session.data['dob-month'] == "4") || (req.session.data['dob-month'] == "04")) {
+      req.session.data['dob-month'] = "April"
+  } else if ((req.session.data['dob-month'] == "5") || (req.session.data['dob-month'] == "05")) {
+      req.session.data['dob-month'] = "May"
+  } else if ((req.session.data['dob-month'] == "6") || (req.session.data['dob-month'] == "06")) {
+      req.session.data['dob-month'] = "June"
+  } else if ((req.session.data['dob-month'] == "7") || (req.session.data['dob-month'] == "07")) {
+      req.session.data['dob-month'] = "July"
+  } else if ((req.session.data['dob-month'] == "8") || (req.session.data['dob-month'] == "08")) {
+      req.session.data['dob-month'] = "August"
+  } else if ((req.session.data['dob-month'] == "9") || (req.session.data['dob-month'] == "09")) {
+      req.session.data['dob-month'] = "September"
+  } else if (req.session.data['dob-month'] == "10") {
+      req.session.data['dob-month'] = "October"
+  } else if (req.session.data['dob-month'] == "11") {
+      req.session.data['dob-month'] = "November"
+  } else if (req.session.data['dob-month'] == "12") {
+      req.session.data['dob-month'] = "December"
+ }
+    
+  if (req.session.data['dob-year'] == "") {
+      res.redirect('/your-details-error-2')
+  }
+  if (req.session.data['NIN'] == "") {
+      res.redirect('/your-details-error-3')
+  }
+
+  if (youDetails == "1"){
+    res.redirect('/your-plea')
+  }
 })
 
 // Your plea
@@ -103,7 +412,6 @@ router.post('/your-plea', function (req, res) {
   }
 
 })
-
 router.post('/your-plea-error', function (req, res) {
 
   var howDoYouPlea = req.session.data['how-do-you-plea']
@@ -134,6 +442,10 @@ router.post('/guilty-plea', function (req, res) {
 
   var guiltyComeToCourt = req.session.data['guilty-come-to-court']
 
+    if (req.session.data['mitigation-textarea'] == "") {
+        req.session.data['mitigation-textarea'] = "–"
+    }
+        
   if (guiltyComeToCourt == "1"){
     req.session.data['how-do-you-plea-2'] = "No";
     res.redirect('/your-finances')
@@ -144,11 +456,27 @@ router.post('/guilty-plea', function (req, res) {
 
 })
 
+// Not guilty plea
+router.post('/not-guilty-plea', function (req, res) {
+    
+    req.session.data['how-do-you-plea-2'] = "Yes - there will be a court hearing when pleading guilty";
+    
+    if (req.session.data['mitigation-textarea'] == "") {
+        req.session.data['mitigation-textarea'] = "–"
+    }
+        
+    res.redirect('/your-finances')
+})
+
 // Your income
 router.post('/your-income', function (req, res) {
 
   var employmentStatus = req.session.data['employment-status-group']
   var benefitsStatus = req.session.data['claiming-benefits-group']
+  
+  if (req.session.data['average-monthly-income'] == "") {
+      req.session.data['average-monthly-income'] = "–"
+  }
 
   if (employmentStatus == "1"){
     req.session.data['employment-status-group'] = "Employed (full or part-time)"
@@ -158,20 +486,26 @@ router.post('/your-income', function (req, res) {
       if (employmentStatus == "1"){
           req.session.data['employment-status-group'] = "Employed (full or part-time)"
       } else if (employmentStatus == "2"){
-          req.session.data['employment-status-group'] = "Self-eemployed"
+          req.session.data['employer-name'] = "–"
+          req.session.data['employment-status-group'] = "Self-employed"
       } else if (employmentStatus == "3"){
+          req.session.data['employer-name'] = "–"
           req.session.data['employment-status-group'] = "Unemployed"
        } else if (employmentStatus == "4"){
+          req.session.data['employer-name'] = "–"
           req.session.data['employment-status-group'] = "Other"
      }
     res.redirect('/your-benefits')
   } else {
       req.session.data['claiming-benefits-group'] = "No"
        if (employmentStatus == "2"){
-          req.session.data['employment-status-group'] = "Self-eemployed"
+           req.session.data['employer-name'] = "–"
+         req.session.data['employment-status-group'] = "Self-eemployed"
       } else if (employmentStatus == "3"){
+          req.session.data['employer-name'] = "–"
           req.session.data['employment-status-group'] = "Unemployed"
        } else if (employmentStatus == "4"){
+          req.session.data['employer-name'] = "–"
           req.session.data['employment-status-group'] = "Other"
      }
      res.redirect('/your-outgoings')
@@ -188,8 +522,11 @@ router.post('/your-employer', function (req, res) {
   if (areYouEmployed == "1"){
     res.redirect('/your-employer-details')
   } else if (benefitsStatus == "1") {
+      req.session.data['claiming-benefits-group'] = "Yes"
       res.redirect('/your-benefits')
   } else {
+      req.session.data['claiming-benefits-group'] = "No"
+      req.session.data['employer-name'] = "You chose to not give employer details"
       res.redirect('/your-outgoings')
   }
 
@@ -199,6 +536,10 @@ router.post('/your-employer', function (req, res) {
 router.post('/your-employer-details', function (req, res) {
 
   var benefitsStatus = req.session.data['claiming-benefits-group']
+  
+  if (req.session.data['employer-name'] == "") {
+      req.session.data['employer-name'] = "–"
+  }
 
   if (benefitsStatus == "1") {
       res.redirect('/your-benefits')
