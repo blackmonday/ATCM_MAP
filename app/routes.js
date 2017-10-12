b17a910ed0e44e6f136bd1ffb0b51e60730ba3b3
Larry Miles <lmiles@lundinoir.com>
2017-10-12 12:28:22 +0100

    can't find the bug

--- app/routes.js
+++ app/routes.js
@@ -99,7 +99,10 @@ router.post('/find-your-case-error-3', function (req, res) {
 // Your details
 router.post('/your-details', function (req, res) {
 
-    //var yourDetails = "1"
+    res.redirect('/your-plea')
+    
+    /*
+    var yourDetails = "1"
     var nameAddressGroup = req.session.data['name-address-group']
     
     if (nameAddressGroup !== "1") {
@@ -109,8 +112,6 @@ router.post('/your-details', function (req, res) {
     }
     
     
-    res.redirect('/your-plea')
-    /*
     if (req.session.data['home-telephone'] == "") {
         req.session.data['home-telephone'] = "–"
     }
